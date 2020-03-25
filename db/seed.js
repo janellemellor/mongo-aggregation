a68e6602d3317f222217641500e5b313f3999304
janelle mellor <janellemellor@gmail.com>
2020-03-25 13:46:31 -0700

    add seed script

---
+++ db/seed.js
@@ -0,0 +1,19 @@
+const Book = require('../lib/models/Book');
+const Author = require('../lib.models/Author');
+const chance = require('chance').Chance();
+
+module.exports = async({ authorsToCreate = 5, booksToCreate = 115 } = {}) => {
+  const authorNames = ['Nayyirah Waheed', 'Adrienne Marie Brown', 'Audre Lorde', 'Chimamanda Ngozi Adichie', 'Yaa Gyasi'];
+
+  const genres = ['horror', 'fiction', 'nonfiction', 'scifi'];
+
+  const authors = await Author.create([...Array(authorsToCreate)].map(()=> ({
+    name: chance.pickone(authorNames)
+  })));
+  
+  await Book.create([...Array(booksToCreate)].map(() => ({
+    authorId: chance.pickone(authors)._id,
+    title: chance.sentence(),
+    genre: chance.pickone(genres)
+  })));
+};
