44db443da8865a05ea2bf300bef22c01dce72307
janelle mellor <janellemellor@gmail.com>
2020-03-25 14:33:29 -0700

    add post test for author route

---
+++ __tests__/authors.test.js
@@ -0,0 +1,24 @@
+const { getAuthor, getAuthors, getBooks } = require('../db/data-helpers');
+const request = requires('supertest');
+const app = require('../lib/app');
+const Author = require('../lib/models/Author');
+const Book = require('../lib/models/Book');
+
+describe('author routes', () => {
+  it('creates an author', () => {
+    return request(app)
+      .post('/api/v1/authors')
+      .send({
+        name: 'test author'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          name: 'test author',
+          __v: 0
+        });
+      });
+  });
+
+
+});
